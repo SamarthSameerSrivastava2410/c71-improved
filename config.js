58bf453a7d246b897b714c790d89d0c14386929a
SamarthSameerSrivastava2410 <72387913+SamarthSameerSrivastava2410@users.noreply.github.com>
2022-03-27 11:01:07 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDNZKggnsSVKQ9-aXsPcoDOWNof2pNKCD8",
+    authDomain: "e-ride-81049.firebaseapp.com",
+    projectId: "e-ride-81049",
+    storageBucket: "e-ride-81049.appspot.com",
+    messagingSenderId: "82235902421",
+    appId: "1:82235902421:web:e54cb1dd4e10fd1d2d155c"
+  };
+  
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
